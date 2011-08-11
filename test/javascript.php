3b8ae62f6a8888aa119473b8eff80e1d6542442f
Dave Artz <dave@artzstudio.com>
2011-08-11 11:36:01 -0400

    Cleaned up stray </head> tags in benchmarks.

--- test/javascript.php
+++ test/javascript.php
@@ -3,8 +3,8 @@
 $expires_offset = 31536000;
 
 // Had to comment these out to ensure IE caches the script.
-// header('Content-Type: application/x-javascript; charset=UTF-8');
-// header('Vary: Accept-Encoding'); // Handle proxies
+//header('Content-Type: application/x-javascript; charset=UTF-8');
+//header('Vary: Accept-Encoding'); // Handle proxies
 header('Expires: ' . gmdate( "D, d M Y H:i:s", time() + $expires_offset ) . ' GMT');
 header("Cache-Control: public, max-age=$expires_offset");
 
